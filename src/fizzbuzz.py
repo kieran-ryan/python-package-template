dcbad8f1634ca35bb6cd578b6fead8b79860e13e
Kieran Ryan <kierankilkenny@gmail.com>
2021-12-11 19:02:50 +0000

    Extracted single fizzbuzz iteration to function

--- src/fizzbuzz.py
+++ src/fizzbuzz.py
@@ -9,14 +9,44 @@ _KEYWORD_MAPPING = {
 }
 
 
-def run(number_limit: int, keyword_mapping: Optional[dict] = None) -> None:
+def fizzbuzz(number: int, keyword_mapping: Optional[dict] = None):
+    """Run FizzBuzz against a number.
+
+    Prints a number, except where printing each keyword mapped to
+    divisible keys in a dictionary.
+
+    Args:
+        number (int): FizzBuzz will run against this integer.
+        keyword_mapping (dict|None): Number -> keyword mapping.
+
+    Returns:
+        None
+    """
+    if keyword_mapping is None:
+        keyword_mapping = _KEYWORD_MAPPING
+
+    output = ""
+
+    for key in keyword_mapping:
+        if number % key == 0:
+            output += keyword_mapping[key]
+
+    if not output:
+        output = str(number)
+
+    return output
+
+
+def fizzbuzz_range(
+    number_limit: int, keyword_mapping: Optional[dict] = None
+) -> None:
     """Run FizzBuzz against a number range.
 
     Print integers 1 to n, except where printing each keyword mapped to
     divisible keys in a dictionary.
 
     Args:
-        number_limit (int): FizzBuzz will be run from 1 to this integer.
+        number_limit (int): FizzBuzz will run from 1 to this integer.
         keyword_mapping (dict|None): Number -> keyword mapping.
 
     Returns:
@@ -26,15 +56,6 @@ def run(number_limit: int, keyword_mapping: Optional[dict] = None) -> None:
         keyword_mapping = _KEYWORD_MAPPING
 
     for number in range(1, number_limit + 1):
-        output = ""
-
-        for key in keyword_mapping:
-            if number % key == 0:
-                output += keyword_mapping[key]
-
-        if not output:
-            output = str(number)
-
-        print(output)
+        print(fizzbuzz(number, keyword_mapping))
 
     return None
