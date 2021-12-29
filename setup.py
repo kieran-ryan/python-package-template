87b3ddc5d1e27c999234bb09a1376d0bc4e33f79
Kieran Ryan <kierankilkenny@gmail.com>
2021-12-29 02:30:07 +0000

    Library build script

---
+++ setup.py
@@ -0,0 +1,36 @@
+"""Library build script."""
+
+import pathlib
+
+import setuptools  # type: ignore
+
+# Extract README content
+repository_base_dir = pathlib.Path(__file__).parent
+README = (repository_base_dir / "README.md").read_text()
+
+# Extract library metadata
+source_base_dir = repository_base_dir / "pysamplelib"
+metadata: dict[str, str] = {}
+with open(source_base_dir / "__version__.py") as version_file:
+    exec(version_file.read(), metadata)
+
+setuptools.setup(
+    name=metadata["__title__"],
+    version=metadata["__version__"],
+    author=metadata["__author__"],
+    description=metadata["__description"],
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url=metadata["__url__"],
+    license=metadata["__license__"],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.10",
+    ],
+    packages=["__title__"],
+    include_package_data=True,
+)
